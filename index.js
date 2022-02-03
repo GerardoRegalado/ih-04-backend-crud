a0900a23e7b179db4c1b99853f8a99666fdedec0
GerardoRegalado <gerardo.regalado24@gmail.com>
2022-02-03 09:36:08 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// importaciones
+const express = require("express")
+const app = express()
+
+
+
+
+//middlewares
+require("dotenv").config
+
+
+
+
+
+//routes
+
+
+
+
+//servidor
+app. listen(process.env.PORT, () => console.log(`servidor activo en ${process.env.PORT}`))
