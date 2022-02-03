c7610b95b806e25c248191b00b329ed468cb7b89
GerardoRegalado <gerardo.regalado24@gmail.com>
2022-02-03 12:50:04 -0600

    creacion de modulo de lectura de libros terminado

---
+++ controllers/bookController.js
@@ -0,0 +1,17 @@
+const Book = require("../models/Book")
+
+exports.getBooks = async (req, res) => {
+
+
+    try{
+    const foundBooks = await Book.find({})
+
+        res.render ("books/list", {
+            data: foundBooks
+        })
+    }
+
+    catch (error) {
+        console.log(error)
+    }
+}
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,2 +1,5 @@
 exports.getHome = (req,res)=> {
     res.send("hola")}
+
+
+    
